748f215183df51eaacc4c010df4310a569cb68f8
Nsubuga Paddy <paddynsubuga48@gmail.com>
2026-02-01 09:14:11 +0300

    change email servers from inmotion to namecheap

--- core/settings.py
+++ core/settings.py
@@ -197,13 +197,14 @@ if DEBUG:
 else:
     EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
     # SSL/TLS settings for mail.mcsug.org (SMTP port 465)
-    EMAIL_HOST = config('EMAIL_HOST', default='mail.mcsug.org')
+    EMAIL_HOST = config('EMAIL_HOST', default='mail.privateemail.com')
     EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
     EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
     EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-    EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='noreply@mcsug.org')
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='noreply@mcs-tgf.com')
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
     DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=config('EMAIL_HOST_USER', default='noreply@mcsug.org'))
+    EMAIL_TIMEOUT = 10  # seconds; avoids worker hanging if SMTP is unreachable
 
 # Session Settings
 SESSION_COOKIE_AGE = 3600  # 1 hour
